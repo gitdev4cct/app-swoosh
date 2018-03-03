cfb6c4f71c8ebbf2f5295dc04f3f8cee430ef14b
Kraken <kraken@Krakens-Mac.local>
2018-03-03 11:06:25 -0800

    final

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Kraken on 3/3/18.
+//  Copyright © 2018 Kraken. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
+
+
